f2b27d39dd8a156eb32aa99016c9b550b755ea68
Andrea Maschio <andrea.maschio@gmail.com>
2017-07-05 14:10:36 +0200

    fixes grafici frame

---
+++ src/main/webapp/scripts/directives/gamebox.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name gatoradeApp.directive:gamebox
+ * @description
+ * # gamebox
+ */
+angular.module('gatoradeApp')
+  .directive('gamebox', function () {
+    return {
+      template: '<iframe id="maingameframe" width="100%" height="100%" scrolling="no" frameborder="0" src="{{game.url}}"></iframe>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+
+          var myIframe = element.find("iframe")[0]
+
+          var script = myIframe.contentWindow.document.createElement("script");
+          script.type = "text/javascript";
+          script.src = "../../bower_components/iframe-resizer/js/iframeResizer.contentWindow.min.js";
+          <!-- iframe resizer-->
+
+          myIframe.onload = function(){
+              myIframe.contentWindow.document.body.appendChild(script);
+              jQuery(document).ready(function () {
+
+                  jQuery('#maingameframe').iFrameResize( [{
+                      log:true,
+                      //enablePublicMethods     : true,
+                      checkOrigin: false,
+                      heightCalculationMethod : 'lowestElement'
+                  }] );
+              });
+          }
+      }
+    };
+  });
+
+
+
