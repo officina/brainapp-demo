7fa1bfd1a49c893e9685f34a13905b40462101f2
Andrea Maschio <andrea.maschio@gmail.com>
2017-07-07 10:08:13 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-mvn package -Pprod -DskipTests
+#!/bin/bash
+if [ $# -eq 0 ]
+    then
+        echo "Usage: build.sh <war name>"
+fi
+./gradlew -Pprod clean bootRepackage
+scp build/libs/$1 officina@10.139.232.201:war
+ssh officina@10.139.232.201 sudo cp war/$1 /var/lib/tomcat8/webapps/
