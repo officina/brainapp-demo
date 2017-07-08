7fa0240e9b9d8f4b5aa493d31fc8723732e02d89
Andrea Maschio <andrea.maschio@gmail.com>
2017-07-08 14:38:36 +0200

    fixes

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 if [ $# -eq 0 ]
     then
         echo "Usage: build.sh <war name>"
+	exit 1
 fi
 ./gradlew -Pprod clean bootRepackage
 scp build/libs/$1 officina@10.139.232.201:war
-ssh officina@10.139.232.201 sudo cp war/$1 /var/lib/tomcat8/webapps/
+ssh officina@10.139.232.201 sudo cp war/$1 /var/lib/tomcat8/webapps/gatorade.war
