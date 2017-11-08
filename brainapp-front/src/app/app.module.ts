5b1c8c1cea6875cc9164c64313784ad60760f0a0
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-08 16:09:16 +0100

    nuked ecom file manager and other stuff, more to come

--- brainapp-front/src/app/app.module.ts
+++ brainapp-front/src/app/app.module.ts
@@ -17,45 +17,36 @@ import { ComponentsModule } from './main/content/components/components.module';
 import { FuseSplashScreenService } from './core/services/splash-screen.service';
 import { FuseConfigService } from './core/services/config.service';
 import { FuseNavigationService } from './core/components/navigation/navigation.service';
-import { ComponentsThirdPartyModule } from './main/content/components-third-party/components-third-party.module';
 import { ServicesModule } from './main/content/services/services.module';
 import { FuseAngularMaterialModule } from './main/content/components/angular-material/angular-material.module';
 import { MarkdownModule } from 'angular2-markdown';
 import { TranslateModule } from '@ngx-translate/core';
 
 const appRoutes: Routes = [
-    // {
-    //     path        : 'apps/mail',
-    //     loadChildren: './main/content/apps/mail/mail.module#FuseMailModule'
-    // },
-    // {
-    //     path        : 'apps/chat',
-    //     loadChildren: './main/content/apps/chat/chat.module#FuseChatModule'
-    // },
+    {
+        path        : 'apps/mail',
+        loadChildren: './main/content/apps/mail/mail.module#FuseMailModule'
+    },
+    {
+        path        : 'apps/chat',
+        loadChildren: './main/content/apps/chat/chat.module#FuseChatModule'
+    },
     {
         path        : 'apps/calendar',
         loadChildren: './main/content/apps/calendar/calendar.module#FuseCalendarModule'
     },
-    // {
-    //     path        : 'apps/e-commerce',
-    //     loadChildren: './main/content/apps/e-commerce/e-commerce.module#FuseEcommerceModule'
-    // },
-    // {
-    //     path        : 'apps/todo',
-    //     loadChildren: './main/content/apps/todo/todo.module#FuseTodoModule'
-    // },
-    // {
-    //     path        : 'apps/file-manager',
-    //     loadChildren: './main/content/apps/file-manager/file-manager.module#FuseFileManagerModule'
-    // },
-    // {
-    //     path        : 'apps/contacts',
-    //     loadChildren: './main/content/apps/contacts/contacts.module#FuseContactsModule'
-    // },
-    // {
-    //     path        : 'apps/scrumboard',
-    //     loadChildren: './main/content/apps/scrumboard/scrumboard.module#FuseScrumboardModule'
-    // },
+    {
+        path        : 'apps/todo',
+        loadChildren: './main/content/apps/todo/todo.module#FuseTodoModule'
+    },
+    {
+        path        : 'apps/contacts',
+        loadChildren: './main/content/apps/contacts/contacts.module#FuseContactsModule'
+    },
+    {
+        path        : 'apps/scrumboard',
+        loadChildren: './main/content/apps/scrumboard/scrumboard.module#FuseScrumboardModule'
+    },
     {
         path      : '**',
         redirectTo: 'apps/dashboards/project'
@@ -90,7 +81,6 @@ const appRoutes: Routes = [
         ServicesModule,
         ComponentsModule,
         FuseAngularMaterialModule,
-        ComponentsThirdPartyModule
     ],
     providers   : [
         FuseSplashScreenService,
