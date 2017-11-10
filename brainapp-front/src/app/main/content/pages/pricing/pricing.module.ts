afcfa93cd8b808311a92855245fe3fc1ac3ed276
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-10 15:23:56 +0100

    tabs on leaderboards are there but with a slider, idfk

--- brainapp-front/src/app/main/content/pages/pricing/pricing.module.ts
+++ brainapp-front/src/app/main/content/pages/pricing/pricing.module.ts
@@ -5,6 +5,10 @@ import { SharedModule } from '../../../../core/modules/shared.module';
 import { FusePricingStyle1Component } from './style-1/style-1.component';
 import { FusePricingStyle2Component } from './style-2/style-2.component';
 import { FusePricingStyle3Component } from './style-3/style-3.component';
+import { ComplessivaComponent } from './style-3/leaderboards/complessiva/complessiva.component';
+import { LogicaComponent } from './style-3/leaderboards/logica/logica.component';
+import { CreativitaComponent } from './style-3/leaderboards/creativita/creativita.component';
+import { ProblemsolvingComponent } from './style-3/leaderboards/problemsolving/problemsolving.component';
 
 const routes = [
     {
@@ -25,7 +29,11 @@ const routes = [
     declarations: [
         FusePricingStyle1Component,
         FusePricingStyle2Component,
-        FusePricingStyle3Component
+        FusePricingStyle3Component,
+        ComplessivaComponent,
+        LogicaComponent,
+        CreativitaComponent,
+        ProblemsolvingComponent
     ],
     imports     : [
         SharedModule,
---
+++ brainapp-front/src/app/main/content/pages/pricing/style-3/leaderboards/problemsolving/problemsolving.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-problemsolving',
+  templateUrl: './problemsolving.component.html',
+  styleUrls: ['./problemsolving.component.scss']
+})
+export class ProblemsolvingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
