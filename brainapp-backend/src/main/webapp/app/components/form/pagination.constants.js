f6c467ddb4c2394179de1b3207e5225833a0610c
Nicola Zambon <nicola.zambon@officina.cc>
2017-11-10 10:28:55 +0100

    Aggiunta progetto di partenza

---
+++ brainapp-backend/src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('brainappbackendApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
