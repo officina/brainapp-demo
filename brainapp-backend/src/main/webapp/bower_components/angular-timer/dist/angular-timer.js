fded3e965260b6bba47df5a2f6cc8eef0d4b4567
Nicola Zambon <nicola.zambon@officina.cc>
2017-11-10 17:02:41 +0100

    rimosso log

--- brainapp-backend/src/main/webapp/bower_components/angular-timer/dist/angular-timer.js
+++ brainapp-backend/src/main/webapp/bower_components/angular-timer/dist/angular-timer.js
@@ -1,9 +1,9 @@
 /**
  * angular-timer - v1.3.5 - 2017-03-09 2:18 PM
  * https://github.com/siddii/angular-timer
- *
- * Copyright (c) 2017 Adrian Wardell
- * Licensed MIT <https://github.com/siddii/angular-timer/blob/master/LICENSE.txt>
+ * 
+ * Copyright (c) 2017 Adrian Wardell Licensed MIT
+ * <https://github.com/siddii/angular-timer/blob/master/LICENSE.txt>
  */
 var timerModule = angular.module('timer', [])
   .directive('timer', ['$compile', function ($compile) {
@@ -43,29 +43,30 @@ var timerModule = angular.module('timer', [])
           };
         }
 
-        //angular 1.2 doesn't support attributes ending in "-start", so we're
-        //supporting both "autostart" and "auto-start" as a solution for
-        //backward and forward compatibility.
+        // angular 1.2 doesn't support attributes ending in "-start", so we're
+        // supporting both "autostart" and "auto-start" as a solution for
+        // backward and forward compatibility.
         $scope.autoStart = $attrs.autoStart || $attrs.autostart;
 
 
         $scope.language = $scope.language || 'en';
         $scope.fallback = $scope.fallback || 'en';
 
-        //allow to change the language of the directive while already launched
+        // allow to change the language of the directive while already launched
         $scope.$watch('language', function(newVal, oldVal) {
           if(newVal !== undefined) {
             i18nService.init(newVal, $scope.fallback);
           }
         });
 
-        //init momentJS i18n, default english
+        // init momentJS i18n, default english
         var i18nService = new I18nService();
         i18nService.init($scope.language, $scope.fallback);
 
-        //progress bar
+        // progress bar
         $scope.displayProgressBar = 0;
-        $scope.displayProgressActive = 'active'; //Bootstrap active effect for progress bar
+        $scope.displayProgressActive = 'active'; // Bootstrap active effect
+													// for progress bar
 
         if ($element.html().trim().length === 0) {
           $element.append($compile('<span>' + $interpolate.startSymbol() + 'millis' + $interpolate.endSymbol() + '</span>')($scope));
@@ -204,7 +205,7 @@ var timerModule = angular.module('timer', [])
 
 
         function calculateTimeUnits() {
-          var timeUnits = {}; //will contains time with units
+          var timeUnits = {}; // will contains time with units
 
           if ($attrs.startTime !== undefined){
             $scope.millis = moment().diff(moment($scope.startTimeAttr));
@@ -256,7 +257,8 @@ var timerModule = angular.module('timer', [])
             $scope.months = Math.floor((($scope.millis / (3600000)) / 24 / 30) % 12);
             $scope.years = Math.floor(($scope.millis / (3600000)) / 24 / 365);
           }
-          // plural - singular unit decision (old syntax, for backwards compatibility and English only, could be deprecated!)
+          // plural - singular unit decision (old syntax, for backwards
+			// compatibility and English only, could be deprecated!)
           $scope.secondsS = ($scope.seconds === 1) ? '' : 's';
           $scope.minutesS = ($scope.minutes === 1) ? '' : 's';
           $scope.hoursS = ($scope.hours === 1) ? '' : 's';
@@ -265,7 +267,8 @@ var timerModule = angular.module('timer', [])
           $scope.yearsS = ($scope.years === 1)? '' : 's';
 
 
-          // new plural-singular unit decision functions (for custom units and multilingual support)
+          // new plural-singular unit decision functions (for custom units and
+			// multilingual support)
           $scope.secondUnit = timeUnits.seconds;
           $scope.minuteUnit = timeUnits.minutes;
           $scope.hourUnit = timeUnits.hours;
@@ -273,7 +276,7 @@ var timerModule = angular.module('timer', [])
           $scope.monthUnit = timeUnits.months;
           $scope.yearUnit = timeUnits.years;
 
-          //add leading zero if number is smaller than 10
+          // add leading zero if number is smaller than 10
           $scope.sseconds = $scope.seconds < 10 ? '0' + $scope.seconds : $scope.seconds;
           $scope.mminutes = $scope.minutes < 10 ? '0' + $scope.minutes : $scope.minutes;
           $scope.hhours = $scope.hours < 10 ? '0' + $scope.hours : $scope.hours;
@@ -283,7 +286,8 @@ var timerModule = angular.module('timer', [])
 
         }
 
-        //determine initial values of time units and add AddSeconds functionality
+        // determine initial values of time units and add AddSeconds
+		// functionality
         if ($scope.countdownattr) {
           $scope.millis = $scope.countdownattr * 1000;
 
@@ -339,10 +343,12 @@ var timerModule = angular.module('timer', [])
           }
           calculateTimeUnits();
 
-          //We are not using $timeout for a reason. Please read here - https://github.com/siddii/angular-timer/pull/5
+          // We are not using $timeout for a reason. Please read here -
+			// https://github.com/siddii/angular-timer/pull/5
           $scope.timeoutId = setTimeout(function () {
               tick();
-              // since you choose not to use $timeout, at least preserve angular cycle two way data binding
+              // since you choose not to use $timeout, at least preserve
+				// angular cycle two way data binding
               // by calling $scope.$apply() instead of $scope.$digest()
               $scope.$apply();
           }, $scope.interval - adjustment);
@@ -367,11 +373,12 @@ var timerModule = angular.module('timer', [])
           }
 
           if(typeTimer !== null){
-            //calculate progress bar
+            // calculate progress bar
             $scope.progressBar = progressBarService.calculateProgressBar($scope.startTime, $scope.millis, $scope.endTime, $scope.countdownattr);
 
             if($scope.progressBar === 100){
-              $scope.displayProgressActive = ''; //No more Bootstrap active effect
+              $scope.displayProgressActive = ''; // No more Bootstrap active
+													// effect
             }
           }
         };
@@ -454,11 +461,13 @@ app.factory('I18nService', function() {
             this.language = this.fallback;
         }
 
-        // It should be handle by the user's application itself, and not inside the directive
+        // It should be handle by the user's application itself, and not inside
+		// the directive
         // moment init
         // moment.locale(this.language);
 
-        //human duration init, using it because momentjs does not allow accurate time (
+        // human duration init, using it because momentjs does not allow
+		// accurate time (
         // momentJS: a few moment ago, human duration : 4 seconds ago
         this.timeHumanizer = humanizeDuration.humanizer({
             language: this.language,
@@ -467,12 +476,20 @@ app.factory('I18nService', function() {
     };
 
     /**
-     * get time with units from momentJS i18n
-     * @param {int} millis
-     * @returns {{millis: string, seconds: string, minutes: string, hours: string, days: string, months: string, years: string}}
-     */
+	 * get time with units from momentJS i18n
+	 * 
+	 * @param {int}
+	 *            millis
+	 * @returns {{millis: string, seconds: string, minutes: string, hours:
+	 *          string, days: string, months: string, years: string}}
+	 */
     I18nService.prototype.getTimeUnits = function getTimeUnits(millis) {
-        var diffFromAlarm = Math.round(millis/1000) * 1000; //time in milliseconds, get rid of the last 3 ms value to avoid 2.12 seconds display
+        var diffFromAlarm = Math.round(millis/1000) * 1000; // time in
+															// milliseconds, get
+															// rid of the last 3
+															// ms value to avoid
+															// 2.12 seconds
+															// display
 
         var time = {};
 
@@ -504,22 +521,28 @@ app.factory('progressBarService', function() {
   var ProgressBarService = function() {};
 
   /**
-   * calculate the remaining time in a progress bar in percentage
-   * @param {momentjs} startValue in seconds
-   * @param {integer} currentCountdown, where are we in the countdown
-   * @param {integer} remainingTime, remaining milliseconds
-   * @param {integer} endTime, end time, can be undefined
-   * @param {integer} coutdown, original coutdown value, can be undefined
-   *
-   * joke : https://www.youtube.com/watch?v=gENVB6tjq_M
-   * @return {float} 0 --> 100
-   */
+	 * calculate the remaining time in a progress bar in percentage
+	 * 
+	 * @param {momentjs}
+	 *            startValue in seconds
+	 * @param {integer}
+	 *            currentCountdown, where are we in the countdown
+	 * @param {integer}
+	 *            remainingTime, remaining milliseconds
+	 * @param {integer}
+	 *            endTime, end time, can be undefined
+	 * @param {integer}
+	 *            coutdown, original coutdown value, can be undefined
+	 * 
+	 * joke : https://www.youtube.com/watch?v=gENVB6tjq_M
+	 * @return {float} 0 --> 100
+	 */
   ProgressBarService.prototype.calculateProgressBar = function calculateProgressBar(startValue, remainingTime, endTimeAttr, coutdown) {
     var displayProgressBar = 0,
       endTimeValue,
       initialCountdown;
 
-    remainingTime = remainingTime / 1000; //seconds
+    remainingTime = remainingTime / 1000; // seconds
 
 
     if(endTimeAttr !== null){
@@ -531,10 +554,14 @@ app.factory('progressBarService', function() {
       displayProgressBar = remainingTime * 100 / coutdown;
     }
 
-    displayProgressBar = 100 - displayProgressBar; //To have 0 to 100 and not 100 to 0
-    displayProgressBar = Math.round(displayProgressBar * 10) / 10; //learn more why : http://stackoverflow.com/questions/588004/is-floating-point-math-broken
+    displayProgressBar = 100 - displayProgressBar; // To have 0 to 100 and not
+													// 100 to 0
+    displayProgressBar = Math.round(displayProgressBar * 10) / 10; // learn
+																	// more why
+																	// :
+																	// http://stackoverflow.com/questions/588004/is-floating-point-math-broken
 
-    if(displayProgressBar > 100){ //security
+    if(displayProgressBar > 100){ // security
       displayProgressBar = 100;
     }
 
