bea43bb097815a1978d19abdcc1ce52cb4a96e18
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-13 14:41:59 +0100

    styling almost completed

---
+++ src/app/main/content/ui/page-layouts/blank/components/creativita/creativita.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creativita',
+  templateUrl: './creativita.component.html',
+  styleUrls: ['./creativita.component.scss']
+})
+export class CreativitaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
