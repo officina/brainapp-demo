5a4b690f54cb095f8a3099fcbe819e38e614cca6
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-16 10:09:54 +0100

    deleted tons of bloat

--- brainapp-front/src/app/app.module.ts
+++ brainapp-front/src/app/app.module.ts
@@ -13,12 +13,10 @@ import { FuseFakeDbService } from './fuse-fake-db/fuse-fake-db.service';
 import { FuseMainModule } from './main/main.module';
 import { PagesModule } from './main/content/pages/pages.module';
 import { UIModule } from './main/content/ui/ui.module';
-import { ComponentsModule } from './main/content/components/components.module';
 import { FuseSplashScreenService } from './core/services/splash-screen.service';
 import { FuseConfigService } from './core/services/config.service';
 import { FuseNavigationService } from './core/components/navigation/navigation.service';
 import { ServicesModule } from './main/content/services/services.module';
-import { FuseAngularMaterialModule } from './main/content/components/angular-material/angular-material.module';
 import { MarkdownModule } from 'angular2-markdown';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -79,8 +77,6 @@ const appRoutes: Routes = [
         PagesModule,
         UIModule,
         ServicesModule,
-        ComponentsModule,
-        FuseAngularMaterialModule,
     ],
     providers   : [
         FuseSplashScreenService,
