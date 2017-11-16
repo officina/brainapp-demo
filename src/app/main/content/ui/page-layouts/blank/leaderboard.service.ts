37374d0da1a545ecaac008be44f0a0b294e5eb76
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-16 14:03:46 +0100

    break for liferay

---
+++ src/app/main/content/ui/page-layouts/blank/leaderboard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
+@Injectable()
+export class LeaderboardService {
+
+  constructor(public http:Http) {
+    console.log('dato');
+  }
+  getScores() {
+      return this.http.get('  https://raw.githubusercontent.com/officina/brainapp/feature/apihook/brainapp-front/global.json?token=ATarJuCkQSNRSV8eOwI5q5qZMSmDUqqlks5aFps5wA%3D%3D')
+      .map(res => res.json());
+  }
+}
+
+
