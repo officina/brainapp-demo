de85125c0da7005478c83ac5e7dd5af0bcfd11e2
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-16 11:38:34 +0100

    ttest

--- src/app/main/content/ui/page-layouts/blank/blank.component.ts
+++ src/app/main/content/ui/page-layouts/blank/blank.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-
+import { LeaderboardService } from './leaderboard.service';
+import * as scores from 'scores';
 @Component({
     selector   : 'fuse-blank',
     templateUrl: './blank.component.html',
     styleUrls  : ['./blank.component.scss']
 })
-export class FuseBlankComponent
-{
-    constructor()
-    {
+export class FuseBlankComponent implements OnInit {
+  score: GlobalScore;
+  constructor(private leaderboardService: LeaderboardService) {}
+    ngOnInit(){
+      this.leaderboardService.getScores().subscribe((score: GlobalScore) => {
+        this.score = score;
+      });
     }
 }
