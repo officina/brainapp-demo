6406f73511be524fcac64aa84a2c18a7a317825c
Alessandro Cappellato <acappellato1@gmail.com>
2017-11-20 14:09:03 +0100

    semiworks

--- src/app/main/content/ui/page-layouts/blank/leaderboard.service.ts
+++ src/app/main/content/ui/page-layouts/blank/leaderboard.service.ts
@@ -8,7 +8,7 @@ import { HttpModule } from '@angular/http';
 export class LeaderboardService {
 
   constructor(public http:Http) {
-    console.log('dato');
+    // console.log('dato');
   }
   getScores() {
       return this.http.get('https://raw.githubusercontent.com/officina/brainapp/feature/apihook/brainapp-front/global.json?token=ATarJuCkQSNRSV8eOwI5q5qZMSmDUqqlks5aFps5wA%3D%3D')
