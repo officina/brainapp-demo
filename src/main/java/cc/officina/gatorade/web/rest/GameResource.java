ea1b67847223d1d159320ccea208840bdaf7cb99
gazz <riccardo.gazzea@officina.cc>
2018-05-16 15:50:59 +0200

    add client http and request

--- src/main/java/cc/officina/gatorade/web/rest/GameResource.java
+++ src/main/java/cc/officina/gatorade/web/rest/GameResource.java
@@ -3,7 +3,6 @@ package cc.officina.gatorade.web.rest;
 import cc.officina.gatorade.domain.enumeration.AttemptSyncState;
 import cc.officina.gatorade.service.*;
 import com.codahale.metrics.annotation.Timed;
-
 import cc.officina.gatorade.domain.Attempt;
 import cc.officina.gatorade.domain.Game;
 import cc.officina.gatorade.domain.Match;
@@ -16,7 +15,10 @@ import cc.officina.gatorade.web.rest.util.HeaderUtil;
 import cc.officina.gatorade.web.rest.util.PaginationUtil;
 import io.swagger.annotations.ApiParam;
 import io.github.jhipster.web.util.ResponseUtil;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -27,9 +29,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -42,7 +46,7 @@ import java.util.Optional;
 public class GameResource {
 
     private final Logger log = LoggerFactory.getLogger(GameResource.class);
-
+    private static final String USER_AGENT = "";
     private static final String ENTITY_NAME = "game";
 
     private final GameService gameService;
@@ -149,8 +153,45 @@ public class GameResource {
         	log.info("Session not valid - not template found for game with id " + id);
         	return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("template", "templateNotFound", "No template founded for game with id "+ id)).body(null);
         }
+        /*boolean validateSession = false;
+        String hostname = "http://gamificationlabcert.generali.it";
+        String endpoint = "/api/gaming/isvalid";
+        String url = hostname+endpoint;
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+
+        // add request header
+        request.addHeader("User-Agent", USER_AGENT);
+        HttpResponse response = null;
+
+        try {
+            log.info("Eseguo chiamata di validazione a: "+url);
+            response = client.execute(request);
+            log.info("Risposta ottenuta: "+response);
+            //log.info("StatusCode: "+response.getStatusLine().getStatusCode());
+            BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            if (response.getStatusLine().getStatusCode() == 200){
+                //TODO verificare la risposta e loggare
+                log.info("Session valid - validation service completed successfully through: " + url);
+                validateSession = sessionService.validateSessionAndUser(extid, playerid, id);
+            }else{
+                log.info("Session not valid - validation service failed through: " + url);
+                validateSession = false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            validateSession = false;
+        }*/
         boolean validateSession = sessionService.validateSessionAndUser(extid, playerid, id);
-        if(! validateSession)
+        if(!validateSession)
         	return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("session", "invalidSession", "Session with ext_id " + extid + " is invalid.")).body(null);
         return new ResponseEntity<>(game, null, HttpStatus.OK);
     }
