51eb7f89b995a7955434b53702989e0588019750
gazz <riccardo.gazzea@officina.cc>
2018-05-25 17:19:21 +0200

    refresh matches

--- src/main/webapp/app/dashboard/dashboard.controller.js
+++ src/main/webapp/app/dashboard/dashboard.controller.js
@@ -11,22 +11,27 @@
         var sessionid = $stateParams.sessions;
         var matches = $stateParams.matches;
 
+        $scope.getMatches = function(){
+            DashboardService.getMatches($stateParams.extsessionid).then(function(response) {
+                $scope.matches = response.data;
+                // console.log(response.data);
+                console.log($scope.matches);
+                $scope.levelgame = isLevelGame($scope.matches[0]) ? true : false;
+                // console.log($scope.levelgame);
+                $scope.matchDuration = $scope.matches[0].template.maxDuration * 1000;
+                $scope.currentDate = new Date();
+                $scope.currentDate = Date.parse($scope.currentDate);
+                // console.log($scope.matchDuration);
+                // console.log(typeof $scope.matches[0].bestLevel)
+            });
+        };
+
+        $scope.getMatches();
+
         DashboardService.getSession($stateParams.extsessionid).then(function(response){
             $scope.sessions = response.data;
         });
 
-        DashboardService.getMatches($stateParams.extsessionid).then(function(response) {
-            $scope.matches = response.data;
-            // console.log(response.data);
-            console.log($scope.matches);
-            $scope.levelgame = isLevelGame($scope.matches[0]) ? true : false;
-            // console.log($scope.levelgame);
-            $scope.matchDuration = $scope.matches[0].template.maxDuration * 1000;
-            $scope.currentDate = new Date();
-            $scope.currentDate = Date.parse($scope.currentDate);
-            // console.log($scope.matchDuration);
-            // console.log(typeof $scope.matches[0].bestLevel)
-        });
         var isLevelGame =function (game){
             // console.log('dentro')
             if (game.game.type === 'LEVEL') {
@@ -36,39 +41,39 @@
                 // console.log('point');
                 return false;
             }
-        }
+        };
 
         $scope.resetMatch = function (matchId) {
             console.log('Sending request for reset match with id: '+matchId);
             DashboardService.resetMatch(matchId)
                 .then(function (response) {
-                    console.log(response)
+                    $scope.getMatches();
                 })
                 .catch(function (error) {
                     console.log(error)
                 })
-        }
+        };
 
         $scope.elaborateMatch = function (matchId) {
             console.log('Sending request for elaborate match with id: '+matchId);
             DashboardService.elaborateMatch(matchId)
                 .then(function (response) {
-                    console.log(response)
+                    $scope.getMatches();
                 })
                 .catch(function (error) {
                     console.log(error)
                 })
-        }
+        };
 
         $scope.closeMatch = function (matchId) {
             console.log('Sending request for close match with id: '+matchId);
             DashboardService.closeMatch(matchId)
                 .then(function (response) {
-                    console.log(response)
+                    $scope.getMatches();
                 })
                 .catch(function (error) {
                     console.log(error)
                 })
-        }
+        };
     }
 })();
