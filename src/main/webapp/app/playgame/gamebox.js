494d426c641dfe5df5dbbf7697d3815347c277ee
gazz <riccardo.gazzea@officina.cc>
2018-07-04 19:00:16 +0200

    fix resize

--- src/main/webapp/app/playgame/gamebox.js
+++ src/main/webapp/app/playgame/gamebox.js
@@ -12,13 +12,11 @@ angular.module('gatoradeApp')
       template: '<iframe id="maingameframe" width="100%" height="100%" scrolling="no" frameborder="0" src="{{game.url}}"></iframe>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-
           var myIframe = element.find("iframe")[0];
           var script = myIframe.contentWindow.document.createElement("script");
           script.type = "text/javascript";
-          script.src = "../iframeResizer.contentWindow.min.js";
+          script.src = "/htmlgames/iframeResizer.contentWindow.min.js";
           <!-- iframe resizer-->
-
           myIframe.onload = function(){
               myIframe.contentWindow.document.body.appendChild(script);
               jQuery(document).ready(function () {
