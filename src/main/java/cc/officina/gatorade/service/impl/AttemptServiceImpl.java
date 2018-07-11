76ade4ba751c6655038150046b4b98625bed59a3
gazz <riccardo.gazzea@officina.cc>
2018-07-11 09:48:33 +0200

    fix minpoint default

--- src/main/java/cc/officina/gatorade/service/impl/AttemptServiceImpl.java
+++ src/main/java/cc/officina/gatorade/service/impl/AttemptServiceImpl.java
@@ -1,5 +1,6 @@
 package cc.officina.gatorade.service.impl;
 
+import cc.officina.gatorade.domain.GameType;
 import cc.officina.gatorade.domain.Match;
 import cc.officina.gatorade.domain.enumeration.AttemptSyncState;
 import cc.officina.gatorade.repository.MatchRepository;
@@ -104,8 +105,16 @@ public class AttemptServiceImpl implements AttemptService{
             if (attempt.getStopAttempt() == null){
                 match.manageAFK(attempt.getLevelReached(), attempt.getAttemptScore(), reqAttempt.getLevelReached(), reqAttempt.getAttemptScore());
             }
+            if (match.getGame().getType() == GameType.MINPOINT){
+                if (reqAttempt.getAttemptScore() < origScore && reqAttempt.getAttemptScore() != 0){
+                    attempt.setAttemptScore(reqAttempt.getAttemptScore());
+                }
+            }else{
+                if (reqAttempt.getAttemptScore() > origScore){
+                    attempt.setAttemptScore(reqAttempt.getAttemptScore());
+                }
+            }
             attempt.setLevelReached(reqAttempt.getLevelReached());
-            attempt.setAttemptScore(reqAttempt.getAttemptScore());
         }else{
             //Cerco un attempt creato "offline" che sia già stato creato su gatorade
             attempt = attemptRepository.getOneByLocalId(reqAttempt.getLocalId());
@@ -127,7 +136,15 @@ public class AttemptServiceImpl implements AttemptService{
                 if (attempt.getStopAttempt() == null){
                     match.manageAFK(attempt.getLevelReached(), attempt.getAttemptScore(), reqAttempt.getLevelReached(), reqAttempt.getAttemptScore());
                 }
-                attempt.setAttemptScore(reqAttempt.getAttemptScore());
+                if (match.getGame().getType() == GameType.MINPOINT){
+                    if (reqAttempt.getAttemptScore() < origScore && reqAttempt.getAttemptScore() != 0){
+                        attempt.setAttemptScore(reqAttempt.getAttemptScore());
+                    }
+                }else{
+                    if (reqAttempt.getAttemptScore() > origScore){
+                        attempt.setAttemptScore(reqAttempt.getAttemptScore());
+                    }
+                }
                 attempt.setLevelReached(reqAttempt.getLevelReached());
             }
         }
--- src/main/java/cc/officina/gatorade/service/impl/GameServiceImpl.java
+++ src/main/java/cc/officina/gatorade/service/impl/GameServiceImpl.java
@@ -256,7 +256,11 @@ public class GameServiceImpl implements GameService{
 	@Override
 	public MatchResponse stopAttempt(Game game, Attempt attempt, boolean completed, Long scoreReached, String levelReached, boolean endMatch) {
         attempt.getMatch().manageAFK(attempt.getLevelReached(), attempt.getAttemptScore(), null, null);
-		attempt.setAttemptScore(scoreReached);
+        if (scoreReached == 0 && game.getType() == GameType.MINPOINT){
+            attempt.setAttemptScore(9999L);
+        }else{
+            attempt.setAttemptScore(scoreReached);
+        }
 		attempt.setLevelReached(levelReached);
 		attempt.setCompleted(completed);
 		attempt.setLastUpdate(ZonedDateTime.now());
