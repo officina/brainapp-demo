aec132efb668a5bda6d170ce6b01e5d5375bbfad
gazz <riccardo.gazzea@officina.cc>
2018-09-07 10:06:02 +0200

    correzione traduzioni e stile

--- brainapp-front/src/app/main/content/pages/pricing/i18n/en.ts
+++ brainapp-front/src/app/main/content/pages/pricing/i18n/en.ts
@@ -6,9 +6,7 @@ export const locale = {
       'RULE_DRAWLINE'         : 'Draw Line rules are simple: fill all the cells in the diagram Explore the various alternatives ' +
                                 'and if you are not satisfied you can always return to yours...',
       'RULE_ELECTRIO'         : 'Opposing poles attract each other, it is a universal law.' +
-                                ' This is a very interesting game in which you need to connect all of the electrons among themselves. ' +
-                                'But remember the rules: 1. You can connect only opposite charges; ' +
-                                '2. Lines must not overlap. You can create the level of your dreams.',
+                                ' This is a very interesting game in which you need to connect all of the electrons among themselves. ',
       'RULE_MAKE7'            : 'Make 7 is a hexagon puzzle game. The goal is to try to make a 7 tile. ' +
                                 'To play drag and drop the tile from the bottom of the screen and put it on the grid.' +
                                 ' You can combine tiles by placing 3 that have the same number next to each other. ' +
@@ -19,8 +17,8 @@ export const locale = {
                                 'The goal is make the red ball coming to the green area. ' +
                                 'You have to pop the green circles....',
       'RULE_THIEF_CHALLENGE'  : 'Control this white-collar thief and try to solve the puzzles to complete his crimes! Find out credit card passwords, ' +
-                                'match keys and try to open locks before running out of time! ' +
-                                'The challenges become increasingly difficult and you must work against the clock and under pressure.'
+                                'match keys and try to open locks before running out of time! ',
+      'GO_CHALLENGE'          : 'GO TO THE CHALLENGE'
     }
   }
 };
